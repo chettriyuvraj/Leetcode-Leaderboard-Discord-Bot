a5072f89ad25f69cb78902fa5724aaf6aa36394e
chettriyuvraj <yuvraj.chettri@quantiphi.com>
2022-09-12 16:46:35 +0530

    1. fix: change deploy-commands file to deploy commands to all servers (instead of only test server)

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
-const { clientId, guildId, token } = require(path.join(__dirname, 'config.json'));
+const { clientId, token } = require(path.join(__dirname, 'config.json'));
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
@@ -16,6 +16,6 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationCommands(clientId), { body: commands })
 	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
 	.catch(console.error);
