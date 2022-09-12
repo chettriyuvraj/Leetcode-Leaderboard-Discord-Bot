c657e472c3c4a349b564bb47e8a53134c47e208e
chettriyuvraj <yuvraj.chettri@quantiphi.com>
2022-09-12 18:02:23 +0530

    1. refactor: use template strings for db config (compatibility issues with raspberry server)

--- sequelize/sequelize.js
+++ sequelize/sequelize.js
@@ -4,7 +4,7 @@ const CONSTANTS = require(path.join(__dirname, '..', 'constants'));
 const LOCALCONSTANTS = CONSTANTS.sequelize;
 const { syncTable } = require(path.join(__dirname, 'syncTable'));
 const { sequelizeDbPassword, sequelizeDbUsername } = require(path.join(__dirname, '..', 'config.json'));
-const sequelize = new Sequelize(`${LOCALCONSTANTS.db}`, `${sequelizeDbUsername}`, `${sequelizeDbPassword}`, LOCALCONSTANTS.config);
+const sequelize = new Sequelize(`postgres://${sequelizeDbUsername}:${sequelizeDbPassword}@localhost/${LOCALCONSTANTS.db}`, LOCALCONSTANTS.config);
 
 const connect = async () => {
 	try {
